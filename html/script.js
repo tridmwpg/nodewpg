e07fb6035ab0ea9cb44683013f16fc075a181e63
Tri Dang <tridang@Tris-MacBook-Pro.local>
2018-03-14 12:37:32 -0500

    Added html exercises files

---
+++ html/script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      alert("Wellcome to Nicolas's Class!!!");
+    });
+  }
